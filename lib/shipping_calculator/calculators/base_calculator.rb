7921ad5a2f29b019ab1f3b03cde91a7c350bb8be
Jan Chruszcz <jrchruszcz@gmail.com>
2024-08-05 12:32:10 +0200

    Base/cheapest-direct calc alpha

---
+++ lib/shipping_calculator/calculators/base_calculator.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'date'
+
+module ShippingCalculator
+  module Calculators
+    class BaseCalculator
+      def initialize(sailing_repository, rate_repository, exchange_rate_repository)
+        @sailing_repository = sailing_repository
+        @rate_repository = rate_repository
+        @exchange_rate_repository = exchange_rate_repository
+      end
+
+      def calculate(origin, destination)
+        raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+      end
+
+      protected
+
+      def find_direct_sailings(origin, destination)
+        @sailing_repository.find_direct_routes(origin, destination)
+      end
+
+      def find_all_routes(origin, destination)
+        @sailing_repository.find_all_routes(origin, destination)
+      end
+
+      def calculate_cost_in_eur(sailing)
+        rate = @rate_repository.find_by_sailing_code(sailing.sailing_code)
+        exchange_rate = @exchange_rate_repository.find_by_date(sailing.departure_date)
+        rate.to_eur(exchange_rate)
+      end
+
+      def calculate_duration(sailing)
+        (Date.parse(sailing.arrival_date) - Date.parse(sailing.departure_date)).to_i
+      end
+
+      def format_result(sailing)
+        rate = @rate_repository.find_by_sailing_code(sailing.sailing_code)
+        {
+          origin_port: sailing.origin_port,
+          destination_port: sailing.destination_port,
+          departure_date: sailing.departure_date,
+          arrival_date: sailing.arrival_date,
+          sailing_code: sailing.sailing_code,
+          rate: rate.rate.to_s,
+          rate_currency: rate.rate_currency
+        }
+      end
+    end
+  end
+end
---
+++ lib/shipping_calculator/calculators/cheapest_direct_calculator.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative 'base_calculator'
+
+module ShippingCalculator
+  module Calculators
+    class CheapestDirectCalculator < BaseCalculator
+      def calculate(origin, destination)
+        direct_sailings = find_direct_sailings(origin, destination)
+        cheapest_sailing = direct_sailings.min_by { |s| calculate_cost_in_eur(s) }
+        format_result(cheapest_sailing)
+      end
+    end
+  end
+end
